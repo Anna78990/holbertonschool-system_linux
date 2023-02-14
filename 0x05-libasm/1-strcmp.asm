4817279dc385a3f969682458f03b3cef5e709a22
Anna78990 <takasuanna1990@gmail.com>
2023-02-14 00:28:51 +0000

    check7

--- 0x05-libasm/1-strcmp.asm
+++ 0x05-libasm/1-strcmp.asm
@@ -2,24 +2,30 @@ global asm_strcmp
 section .text
 
 asm_strcmp:
-            xor rax, rax
-            mov rax, [rdi]
-            cmp rax, [rsi]
-            je equal
-	    jl less
-	    jg greater
+    push rbp
+    mov rbp, rsp
+    mov rcx, 0
+compare:
+    xor rax, rax
+    xor rbx, rbx
+    mov al, byte [rdi + rcx]
+    mov bl, byte [rsi + rcx]
+    cmp al, bl
+    je equal
+    jl less
+    jg greater
 equal:
-	    cmp rax, 0
-	    je exit
-	    inc rdi
-	    inc rsi
-	    jmp asm_strcmp
-
+    cmp al, 0
+    je exit
+    inc rcx
+    jmp compare
 less:
-	    mov rax, -1
-	    jmp exit
+    mov rax, -1
+    jmp exit
 greater:
-	    mov rax, 1
-	    jmp exit
+    mov rax, 1
+    jmp exit
 exit:
-	    ret
+    mov rsp, rbp
+    pop rbp
+    ret
