323cfb3009eeb3b313a073a32e1cd4373dad49ac
Anna78990 <takasuanna1990@gmail.com>
2023-06-25 08:44:06 +0000

    INT

--- 0x01-getline/laps.c
+++ 0x01-getline/laps.c
@@ -10,11 +10,11 @@
  */
 void _sort(int *arr, int size)
 {
-	int swap, swapped;
+	int i, j, swap, swapped;
 
-	for (int i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (int j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (arr[j] > arr[j + 1])
 			{
