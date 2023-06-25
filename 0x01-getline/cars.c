477a6e55952b5a9698af6afdf53187df3ccdafd3
Anna78990 <takasuanna1990@gmail.com>
2023-06-25 09:58:05 +0000

    debug versin

--- 0x01-getline/cars.c
+++ 0x01-getline/cars.c
@@ -1,3 +1,6 @@
+why I could not get expected out put on following code?:
+
+laps.c:
 #include <stdio.h>
 #include <stddef.h>
 #include "laps.h"
@@ -58,49 +61,36 @@ listcars_t *add_car(listcars_t **cars, int *list, size_t size)
 
 	for (i = 0; i < (int)size; i++)
 	{
-		if (*cars)
-			printf("in this iteration, cars->id is %d\n", (*cars)->id);
 		new = malloc(sizeof(listcars_t));
 		new->id = list[i], new->laps = -1, new->next = NULL;
 		if (*cars == NULL)
 		{
-			printf("im in if\n");
 			*cars = new;
 			head = new;
 		}
 		else
 		{
-			printf("im in else\n");
-			tmp = head = *cars;
+			tmp = *cars;
 			while (*cars)
 			{
-				printf("im in while, cars->id is %d\n", (*cars)->id);
 				if ((*cars)->id > list[i])
 				{
 					if ((*cars) == tmp)
 					{
 						new->next = *cars;
 						tmp = head = *cars = new;
-						break;
 					}
 					else
 					{
 						tmp->next = new;
 						new->next = *cars;
-						*cars = head;
-						break;
 					}
 				}
 				tmp = *cars;
 				*cars = (*cars)->next;
 			}
-			if (tmp)
-				printf("tmp id is %d\n", tmp->id);
 			if ((*cars) == NULL)
-			{
 				tmp->next = new;
-				printf("cars is NULL\n");
-			}
 		}
 		printf("Car %d joined the race\n", new->id);
 	}
@@ -167,3 +157,132 @@ int search_carslist(listcars_t **cars, int *list, size_t size)
 	}
 	return (0);
 }
+
+
+0-main2.c: 
+#include "laps.h"
+
+/**
+ * main - entry point.
+ *
+ * Return: always 0.
+ */
+int main()
+{
+
+    int ids[3] = {89, 2, 102};
+    int i;
+    
+    for (i = 0 ; i < 10 ; i ++) 
+    {
+      race_state(ids, 3);
+    }
+    race_state(NULL, 0);
+    return (0);
+}
+
+
+laps.h:
+#ifndef LAPS_H
+#define LAPS_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listcars_s - singly linked list
+ * @id: id
+ * @laps: laps
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listcars_s
+{
+	int id;
+	int laps;
+	struct listcars_s *next;
+} listcars_t;
+
+
+void race_state(int *id, size_t size);
+listcars_t *add_car(listcars_t **cars, int *list, size_t size);
+listcars_t *change_state(listcars_t **cars, int *list, size_t size);
+int search_carslist(listcars_t **cars, int *list, size_t size);
+
+#endif
+
+compile command:
+vagrant@vagrant:~/holbertonschool-system_linux/0x01-getline$ gcc -Wall -Wextra -Werror -pedantic 0-main2.c laps.c  -o laps
+
+[got]
+laps
+vagrant@vagrant:~/holbertonschool-system_linux/0x01-getline$ ./laps 
+Car 89 joined the race
+Car 2 joined the race
+Car 102 joined the race
+Race state:
+Car 102 [0 laps]
+Race state:
+Car 102 [1 laps]
+Race state:
+Car 102 [2 laps]
+Race state:
+Car 102 [3 laps]
+Race state:
+Car 102 [4 laps]
+Race state:
+Car 102 [5 laps]
+Race state:
+Car 102 [6 laps]
+Race state:
+Car 102 [7 laps]
+Race state:
+Car 102 [8 laps]
+Race state:
+Car 102 [9 laps]
+
+[Expected]
+Car 89 joined the race
+Car 2 joined the race
+Car 102 joined the race
+Race state:
+Car 2 [0 laps]
+Car 89 [0 laps]
+Car 102 [0 laps]
+Race state:
+Car 2 [1 laps]
+Car 89 [1 laps]
+Car 102 [1 laps]
+Race state:
+Car 2 [2 laps]
+Car 89 [2 laps]
+Car 102 [2 laps]
+Race state:
+Car 2 [3 laps]
+Car 89 [3 laps]
+Car 102 [3 laps]
+Race state:
+Car 2 [4 laps]
+Car 89 [4 laps]
+Car 102 [4 laps]
+Race state:
+Car 2 [5 laps]
+Car 89 [5 laps]
+Car 102 [5 laps]
+Race state:
+Car 2 [6 laps]
+Car 89 [6 laps]
+Car 102 [6 laps]
+Race state:
+Car 2 [7 laps]
+Car 89 [7 laps]
+Car 102 [7 laps]
+Race state:
+Car 2 [8 laps]
+Car 89 [8 laps]
+Car 102 [8 laps]
+Race state:
+Car 2 [9 laps]
+Car 89 [9 laps]
+Car 102 [9 laps]
