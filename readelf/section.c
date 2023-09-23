0be8cb320a06a6b77251839fc8be9c0a8540ae1d
Anna78990 <takasuanna1990@gmail.com>
2023-09-23 17:01:36 +0000

    del last \n

--- readelf/section.c
+++ readelf/section.c
@@ -60,9 +60,7 @@ int print_section_headers(header *h, int fd)
 		print_section64(h, str);
 	printf("%s", KEY_FLAG);
 	if (h->e64.e_ident[EI_CLASS] == ELFCLASS64)
-		printf(", l (large)\n");
-	else
-		printf("\n");
+		printf(", l (large)");
 	free(str);
 	return (0);
 }
