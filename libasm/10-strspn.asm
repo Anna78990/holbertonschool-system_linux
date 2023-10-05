2acb88bebdf0281ec993fe687a8d9d3aa41b2ba1
Anna78990 <takasuanna1990@gmail.com>
2023-10-05 15:46:51 +0000

    10-asm

--- libasm/10-strspn.asm
+++ libasm/10-strspn.asm
@@ -1,48 +1,54 @@
-section .text
 global asm_strspn
+section .text
 
 asm_strspn:
-    push rbp
-    mov rbp, rsp
-    push rcx
-    xor rax, rax
-    jmp loop
+	push rbp
+	mov rbp, rsp
+	mov rcx, 0
 
-loop:
-    mov cl, byte [rsi + rax]
-    test cl, cl
-    jz end
-    mov dl, byte [rdi + rax]
-    test dl, dl
-    jz end
-    cmp cl, dl
-    jne end
-    inc rax
-    jmp loop
+loop_strspn:
+	cmp byte [rdi], 0
+	jz strspn_end
+	mov rax, 0
+	push rsi
+	push rdi
+	mov rdi, rsi
+	mov rsi, [rsp]
+	movzx rsi, byte [rsi]
+	jmp strchr
+back:
+	pop rdi
+	pop rsi
+	test rax, rax
+	jz strspn_end
+	inc rcx
+	inc rdi
+	jmp loop_strspn
 
-end:
-    ; Print the intermediate values
-    push rax
-    push rdi
-    push rsi
-    call print_values
-    add rsp, 12
+strspn_end:
+	mov rax, rcx
+	mov rsp, rbp
+	pop rbp
+	ret
 
-    pop rcx
-    mov rsp, rbp
-    pop rbp
-    ret
+strchr:
+	push rbp
+	mov rbp, rsp
+	mov rax, 0
 
-print_values:
-    ; Print the values stored in rax, rdi, rsi registers
-    mov rdi, rax
-    mov rax, 1
-    mov rsi, format
-    mov rdx, format_len
-    syscall
-    ret
+strchr_loop:
+	mov al, byte [rdi]
+	cmp byte [rdi], sil
+	jz found
+	cmp byte [rdi], 0
+	jz strchr_end
+	inc rdi
+	jmp strchr_loop
 
+found:
+	mov rax, rdi
 
-section .data
-format db "%zu, %zu, %zu", 10
-format_len equ $ - format
+strchr_end:
+	mov rsp, rbp
+	pop rbp
+	jmp back
