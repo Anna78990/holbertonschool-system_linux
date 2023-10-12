78b667fe7565ae912f28e20d2a3d6b82a3c46d14
Anna78990 <takasuanna1990@gmail.com>
2023-10-12 14:41:34 +0000

    10-pid_exist

---
+++ signals/10-pid_exist.c
@@ -0,0 +1,11 @@
+#include "signals.h"
+
+/**
+ * pid_exist - tests if a process exists, given its PID
+ * @pid: process ID
+ * Return: 1 if the process with the PID pid exists, or 0 otherwise
+ */
+int pid_exist(pid_t pid)
+{
+	return ((kill(pid, 0) == 0) ? 1 : 0);
+}
--- signals/signals.h
+++ signals/signals.h
@@ -12,5 +12,6 @@ void set_print_hello(void);
 int handle_sigaction(void);
 void (*current_handler_sigaction(void))(int);
 int trace_signal_sender(void);
+int pid_exist(pid_t pid);
 
 #endif
