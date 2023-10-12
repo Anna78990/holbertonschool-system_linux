80bf4ea4f7991b9335409715fce55c9e74a15913
Anna78990 <takasuanna1990@gmail.com>
2023-10-12 13:08:00 +0000

    7-signal_send

---
+++ signals/7-signal_send.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+/**
+ * main - it sends the signal SIGINT to a process, given its PID
+ * @argc: argc
+ * @argv: argv
+ * Return: Exit status
+ */
+int main(int argc, char *argv[])
+{
+	pid_t target_pid = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Usage: %s <pid>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+
+	if (kill(target_pid, SIGINT) == 0)
+	{
+		return (EXIT_SUCCESS);
+	}
+	else
+	{
+		perror("kill");
+		return (EXIT_FAILURE);
+	}
+}
