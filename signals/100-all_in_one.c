cc57348b1cd0edf9c3a81c8d524ecac6b6e8a61d
Anna78990 <takasuanna1990@gmail.com>
2023-10-20 14:41:10 +0000

    100

---
+++ signals/100-all_in_one.c
@@ -0,0 +1,30 @@
+#include "signals.h"
+#include <string.h>
+
+/**
+ * signal_handler - signal hundler
+ * @signum: signal number
+ * @siginfo: pointer to struct signal info
+ * @ucontext: pointer to struct ucontext
+ */
+void signal_handler(__attribute__((unused)) int signum,
+		siginfo_t *siginfo, __attribute__((unused))void *ucontext)
+{
+	psiginfo(siginfo, "Caught!");
+}
+
+/**
+ * all_in_one - sets up a single handler for all the signals
+ */
+void all_in_one(void)
+{
+	struct sigaction sa;
+	int signum;
+
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = signal_handler;
+
+	for (signum = 0; signum < NSIG; signum++)
+		sigaction(signum, &sa, NULL);
+}
