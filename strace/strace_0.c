67742ba7d31637f496d3da1ddb8f8a33f0c8fe40
Anna78990 <takasuanna1990@gmail.com>
2023-12-07 09:31:49 +0000

    0

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -1,103 +1,79 @@
-#include "strace.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <sys/user.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
 
-#include "strace.h"
 
 /**
- * trace_syscall - Start or stop the process at the next entry or exit from a system call.
- * @pid: Process ID to trace.
- *
- * Return: 1 if the process is stopped by a signal, 0 if the process has exited.
+ * syscall_await - waits for a syscall
+ * @child: pid of child process to await
+ * Return: 0 if child stopped, 1 if exited
  */
-int trace_syscall(pid_t pid)
+int syscall_await(pid_t child)
 {
 	int status;
 
 	while (1)
 	{
-		ptrace(PTRACE_SYSCALL, pid, NULL, NULL);
-		waitpid(pid, &status, 0);
+		ptrace(PTRACE_SYSCALL, child, 0, 0);
+		waitpid(child, &status, 0);
 		if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80)
-			return 1;
+			return (0);
 		if (WIFEXITED(status))
-			return 0;
+			return (1);
 	}
-}
-
-/**
- * validate_args - Check for usage errors.
- * @argc: Number of command-line arguments.
- * @argv: Pointer to an array of character strings that contain the arguments.
- *
- * Return: 0 on success, 1 on error.
- */
-int validate_args(int argc, char *argv[])
-{
-	struct stat sb;
-
-	if (argc < 2)
-	{
-		fprintf(stderr, "%s command [args...]\n", *argv);
-		return 1;
-	}
-	if (stat(argv[1], &sb) == -1)
-	{
-		fprintf(stderr, "%s: Can't stat '%s': No such file or directory\n",
-				*argv, argv[1]);
-		return 1;
-	}
-	return 0;
 }
 
 
 /**
- * main - Entry point.
- * @argc: Number of command-line arguments.
- * @argv: Pointer to an array of character strings that contain the arguments.
- *
- * Return: 0 on success, 1 on failure.
+ * main - executes and traces a given command
+ * @argc: number of args
+ * @argv: arguments
+ * Return: 0 if succeed, otherwise exit status
  */
 int main(int argc, char *argv[])
 {
-    pid_t child_pid, parent_pid;
+	int status;
+	pid_t child;
+	struct user_regs_struct regs;
 
-    if (validate_args(argc, argv))
-        return 1;
-
-    parent_pid = getpid();
-    child_pid = fork();
-
-    if (child_pid == -1)
-    {
-        perror("fork");
-        return 1;
-    }
-    else if (child_pid == 0)
-    {
-        if (ptrace(PTRACE_TRACEME) == -1)
-        {
-            perror("ptrace");
-            return 1;
-        }
-
-        kill(parent_pid, SIGSTOP);
-
-        return execvp(argv[1], argv + 1);
-    }
-    else
-    {
-	    int status;
- 
- 	    setbuf(stdout, NULL);
-            waitpid(child_pid, &status, 0);
-            ptrace(PTRACE_SETOPTIONS, child_pid, 0, PTRACE_O_TRACESYSGOOD);
-            while (1)
-            {
-                 if (!trace_syscall(child_pid))
-                     break;
-                 printf("%li\n", ptrace(PTRACE_PEEKUSER, child_pid, sizeof(long) * ORIG_RAX));
-                 if (!trace_syscall(child_pid))
-                     break;
-            }
-        return 0;
-    }
+	if (argc < 2)
+		exit(EXIT_FAILURE);
+	child = fork();
+	if (child == -1)
+	{
+		dprintf(STDERR_FILENO, "Fork failed: %d\n", errno);
+		exit(-1);
+	}
+	else if (!child)
+	{
+		ptrace(PTRACE_TRACEME, 0, 0, 0);
+		kill(getpid(), SIGSTOP);
+		if (execv(argv[1], argv + 1) == -1)
+		{
+			dprintf(STDERR_FILENO, "Exec failed: %d\n", errno);
+			exit(-1);
+		}
+	}
+	else
+	{
+		waitpid(child, &status, 0);
+		ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD);
+		while (1)
+		{
+			if (syscall_await(child))
+				break;
+			memset(&regs, 0, sizeof(regs));
+			ptrace(PTRACE_GETREGS, child, 0, &regs);
+			printf("%lu\n", (long)regs.orig_rax);
+			if (syscall_await(child))
+				break;
+		}
+	}
+	return (0);
 }
