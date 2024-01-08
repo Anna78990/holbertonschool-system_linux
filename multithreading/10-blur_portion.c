ff80d29eeb3472f3ab814b662a4fa6ff373c4fc0
Anna78990 <takasuanna1990@gmail.com>
2024-01-08 06:41:42 +0000

    10

--- multithreading/10-blur_portion.c
+++ multithreading/10-blur_portion.c
@@ -1,98 +1,87 @@
 #include "multithreading.h"
 
 /**
- * get_sums - initialise sums for rgb and kernel
- * @r_sum: pointer to sum of red values
- * @g_sum: pointer to sum of green values
- * @b_sum: pointer to sum of blue values
- * @kernel_sum:	pointer to sum of kernel values
- * @portion: pointer to structure with information needed to blur
- * @pixels: double array of pixels
- * @x: x position for pixels array of pixel to blur
- * @y: y position for pixels array of pixel to blur
+ * free_array - free the given arrays
+ * @h: size of array
+ * @pix: array to free
  */
-void get_sums(float *r_sum, float *g_sum, float *b_sum, float *kernel_sum,
-		const blur_portion_t *portion, const pixel_t **pixels,
-		size_t x, size_t y)
+void free_array(size_t h, pixel_t ***pix)
 {
-	ssize_t grid_x, grid_y, grid_stop_x, grid_stop_y;
-	size_t half_kernel, kernel_x, kernel_y;
+	size_t i;
 
-	half_kernel = portion->kernel->size / 2;
-	grid_x = x - half_kernel;
-	grid_y = y - half_kernel;
-	grid_stop_y = grid_y + portion->kernel->size;
-	grid_stop_x = grid_x + portion->kernel->size;
-	for (grid_y = y - half_kernel,
-			kernel_y = *r_sum = *g_sum = *b_sum = *kernel_sum = 0;
-			grid_y < grid_stop_y; grid_y++, kernel_y++)
-	{
-		for (grid_x = x - half_kernel, kernel_x = 0; grid_x < grid_stop_x;
-				grid_x++, kernel_x++)
-		{
-			if (grid_x > -1 && grid_y > -1 &&
-					grid_x < (ssize_t) portion->img->w &&
-					grid_y < (ssize_t) portion->img->h)
-			{
-				*r_sum += portion->kernel->matrix[kernel_y][kernel_x] *
-					pixels[grid_y][grid_x].r;
-				*g_sum += portion->kernel->matrix[kernel_y][kernel_x] *
-					pixels[grid_y][grid_x].g;
-				*b_sum += portion->kernel->matrix[kernel_y][kernel_x] *
-					pixels[grid_y][grid_x].b;
-				*kernel_sum += portion->kernel->matrix[kernel_y][kernel_x];
-			}
-		}
-	}
+	for (i = 0; i < h; i++)
+		free(pix[i]);
+	free(pix);
 }
 
 /**
- * blur_pixel - blur individual pixel
- *
+ * conv - convlusions for rgb and kernel
  * @portion: pointer to structure with information needed to blur
- * @pixels: double array of pixels
- * @x: x position for pixels array of pixel to blur
- * @y: y position for pixels array of pixel to blur
- * @pidx: index of pixel to blur for output array
- *
- * Using pixels double array for simplified 1 to 1 traversal of image with
- * kernel
+ * @i: start of height
+ * @j: start of width
+ * @img: double array of pixels
+ * Return: convluisoned pixel
  */
-void blur_pixel(const blur_portion_t *portion, const pixel_t **pixels,
-		size_t x, size_t y, size_t pidx)
+pixel_t *conv(blur_portion_t const *portion, size_t i, size_t j,
+		pixel_t ***img)
 {
-	float r_avg, g_avg, b_avg, kernel_sum;
+	pixel_t *new_pixel = NULL;
+	float sum = 0.0, **kernel = NULL;
+	float red = 0.0, blue = 0.0, green = 0.0;
+	ssize_t size_kernel = 0, mid = 0, start_x = 0, start_y = 0,
+		end_x = 0, end_y = 0;
 
-	get_sums(&r_avg, &g_avg, &b_avg, &kernel_sum, portion, pixels, x, y);
-	r_avg /= kernel_sum, g_avg /= kernel_sum, b_avg /= kernel_sum;
-	portion->img_blur->pixels[pidx].r = r_avg;
-	portion->img_blur->pixels[pidx].g = g_avg;
-	portion->img_blur->pixels[pidx].b = b_avg;
+	new_pixel = calloc(1, sizeof(pixel_t));
+	size_kernel = portion->kernel->size;
+	mid = size_kernel / 2;
+	kernel = portion->kernel->matrix;
+
+	end_y = i + mid;
+	end_x = j + mid;
+	for (start_y = i - mid; start_y <= end_y; start_y++)
+	{
+		for (start_x = j - mid; start_x <= end_x; start_x++)
+		{
+			if (start_y > -1 && start_x > -1 &&
+					start_x < (ssize_t) portion->img->w &&
+					start_y < (ssize_t) portion->img->h)
+			{
+				red += (img[start_y][start_x]->r *
+						kernel[start_y - i + mid][start_x - j + mid]);
+				blue += (img[start_y][start_x]->b *
+						kernel[start_y - i + mid][start_x - j + mid]);
+				green += (img[start_y][start_x]->g *
+						kernel[start_y - i + mid][start_x - j + mid]);
+				sum += kernel[start_y - i + mid][start_x - j + mid];
+			}
+		}
+	}
+	new_pixel->r = red / sum;
+	new_pixel->b = blue / sum;
+	new_pixel->g = green / sum;
+
+	return (new_pixel);
 }
 
 /**
  * create_array - create dimentional pixel_t array from an original array
  * @img: pointer to img struct containing pixel_t array
- *
+ * @pxl: pointer to pixel to refer
  * Return: created array or NULL if malloc fails
  */
-pixel_t **create_array(const img_t *img)
+pixel_t ***create_array(const img_t *img, pixel_t *pxl)
 {
-	pixel_t **pixels;
-	size_t i, j, k = 0;
+	size_t i, j;
+	pixel_t ***pixels;
 
-	if (!img || img->w == 0 || img->h == 0)
-		return (NULL);
-	pixels = malloc(img->h * sizeof(*pixels));
-	if (!pixels)
-		return (NULL);
+	pixels = calloc(img->h, sizeof(img_t **));
+	for (i = 0; i < img->h; i++)
+		pixels[i] = calloc(img->w, sizeof(img_t *));
+
 	for (i = 0; i < img->h; i++)
 	{
-		pixels[i] = malloc(img->w * sizeof(**pixels));
-		if (!pixels[i])
-			return (NULL);
-		for (j = 0; j < img->w; j++, k++)
-			pixels[i][j] = img->pixels[k];
+		for (j = 0; j < img->w; j++)
+			pixels[i][j] = &(pxl[(img->w * i) + j]);
 	}
 	return (pixels);
 }
@@ -103,33 +92,24 @@ pixel_t **create_array(const img_t *img)
  */
 void blur_portion(blur_portion_t const *portion)
 {
-	size_t i, j, img_idx, start, stop_x, stop_y;
-	pixel_t **array_pixels;
+	pixel_t ***blur_img = NULL, ***img = NULL, *conved = NULL;
+	size_t i, j;
 
 	if (!portion)
 		return;
-	array_pixels = create_array(portion->img);
-	if (!array_pixels)
-		return;
-	start = portion->x + (portion->y * portion->img->w);
-	img_idx = start;
-	stop_x = start + portion->w;
-	stop_y = stop_x + (portion->img->w * (portion->h - 1));
-	for (i = portion->x; i < portion->w + portion->x; i++)
+	blur_img = create_array(portion->img, portion->img_blur->pixels);
+	img = create_array(portion->img, portion->img->pixels);
+	for (i = portion->y; i < portion->y + portion->h; i++)
 	{
-		for (j = portion->y; j < portion->h + portion->y; j++)
+		for (j = portion->x; j < portion->x + portion->w; j++)
 		{
-			blur_pixel(portion, (const pixel_t **)array_pixels, i, j, img_idx);
-			img_idx += portion->img->w;
-			if (img_idx >= stop_y)
-			{
-				img_idx += 1;
-				if (img_idx >= stop_x)
-					break;
-			}
+			conved = conv(portion, i, j, img);
+			(blur_img[i][j])->r = conved->r;
+			(blur_img[i][j])->b = conved->b;
+			(blur_img[i][j])->g = conved->g;
+			free(conved);
 		}
 	}
-	for (i = 0; i < portion->img->h; i++)
-		free(array_pixels[i]);
-	free(array_pixels);
+	free_array(portion->img->h, blur_img);
+	free_array(portion->img->h, img);
 }
